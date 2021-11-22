abd985e8d441b1abc074d9651757adceb4342823
sarahmoe <79814626+s-moe@users.noreply.github.com>
2021-11-22 21:25:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//slice
+
+let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple' ]
+
+console.log(colors.slice(3))
+
+console.log(colors)
+
+//first element is starting point, second stops up to that element
+
+console.log(colors.slice(0,3))
